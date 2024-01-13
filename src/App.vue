31d14e338f79d5d811897d40a2e7d5c7dfcce627
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-01-13 11:50:45 +0300

    Add Tailwind CSS and PostCSS configuration files

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Navbar from '@/views/Navbar.vue';
+import Carousel from '@/views/Carousel.vue';
+import Video from '@/views/Video.vue';
+
+import { useMovieStore } from './stores/movie'
+import { storeToRefs } from 'pinia';
+const useMovie = useMovieStore()
+const {showFullVideo } = storeToRefs(useMovie)
+</script>
+
+<template>
+  <div class="fixed w-full h-screen bg-black">
+
+    <div v-if="!showFullVideo">
+      <Navbar />
+    </div>
+
+    <Carousel />
+    
+    </div>
+
+    <Video />
+
+</template>
